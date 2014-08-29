00446895454d422ad4adfc3f031af322821726dc
R. S. Doiel <rsdoiel@gmail.com>
2014-08-29 09:17:11 -0700

    Update rutils.js

--- rutils.js
+++ rutils.js
@@ -3,41 +3,47 @@
  * @author R. S. Doiel, <rsdoiel@gmail.com>
  */
 /*jslint browser: true, indent: 4 */
+/*global console */
 (function (window, document) {
-  "use strict";
+    "use strict";
   
-   function urlSearchParams(url_as_string) {
-    var URLUtils = window.URL || window.webkitURL,
-        url = new URLUtils(url_string),
-        query_string,
-        fields,
-        key_values = {};
+    function urlSearchParams(url_as_string) {
+        var url = new window.URL(url_as_string),
+            query_string = "",
+            raw_pair = "",
+            pair = [],
+            key = "",
+            value = "",
+            i = 0,
+            fields = [],
+            key_values = {};
     
-    if (url.search !== "" && url.search.indexOf('?') === 0) {
-        query_string = url.search.substr('1');
-        if (query_string.indexOf('&') !== -1) {
-            fields = query_string.split('&');
-        } else {
-            fields = [query_string];
-        }
-        fields.foreach(function (raw_pair) {
-            var pair = raw_pair.split('=', 2),
-                key, value;
-            
-            if (pair.length === 2) {
-                key = decodeURIComponent(pair[0]);
-                value = decodeURIComponent(pair[1]);
-                key_values[key] = value;
+        if (url.search !== "" && url.search.indexOf('?') === 0) {
+            query_string = url.search.substr('1');
+            if (query_string.indexOf('&') !== -1) {
+                fields = query_string.split('&');
+            } else {
+                fields = [query_string];
             }
-        });
+            for (i = 0; i < fields.length; i += 1) {
+                raw_pair = fields[i];
+                if (raw_pair.indexOf('=') !== -1) {
+                    pair = raw_pair.split('=', 2);
+                    if (pair.length === 2) {
+                        key = decodeURIComponent(pair[0]);
+                        value = decodeURIComponent(pair[1]);
+                        key_values[key] = value;
+                    }
+                }
+            }
+        }
+        return key_values;
     }
-    return key_values;
-   }
    
    
-   if (window.RUTIL === undefined) {
-      window.RUTIL = {
-          urlSearchParams: urlSearchParams
-      };
-   }
+    if (window.RUTIL === undefined) {
+        window.RUTIL = {
+            urlSearchParams: urlSearchParams
+        };
+    }
 }(window, document));
